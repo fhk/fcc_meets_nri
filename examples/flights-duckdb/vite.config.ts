30c9df093cb0e0dfe8df5be84c2a1b26eec84f9f
fhk <4031392+fhk@users.noreply.github.com>
2025-07-14 17:05:59 -0700

    update deps

--- examples/flights-duckdb/vite.config.ts
+++ examples/flights-duckdb/vite.config.ts
@@ -4,6 +4,16 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	build: {
+		rollupOptions: {
+			external: [
+				"fast-deep-equal",
+				// Add other modules here if you encounter similar "failed to resolve"
+				// errors for them during the build process, *after* confirming
+				// they are installed and should be external.
+			],
+		},
+	},
 	optimizeDeps: {
 		include: [
 			"fast-deep-equal",
